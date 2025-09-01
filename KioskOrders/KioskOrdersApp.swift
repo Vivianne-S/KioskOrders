f5a10bf31573ab3bffb97f782a7f0f5b19ebd054
Vivianne-s <vivvi.sonnerborg@hotmail.com>
2025-09-01 12:22:59 +0200

    Initial Commit

---
+++ KioskOrders/KioskOrdersApp.swift
@@ -0,0 +1,17 @@
+//
+//  KioskOrdersApp.swift
+//  KioskOrders
+//
+//  Created by Vivianne Sonnerborg on 2025-09-01.
+//
+
+import SwiftUI
+
+@main
+struct KioskOrdersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
